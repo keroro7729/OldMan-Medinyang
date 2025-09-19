831a84ee37aaa9d7d47556f6ba36fe079f7e3cdb
keroro7729 <keroro06108@gmail.com>
2025-09-20 04:36:41 +0900

    임시

---
+++ medinyang/src/main/java/oldman/medinyang/domain/ExampleUserData.java
@@ -0,0 +1,10 @@
+package oldman.medinyang.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class ExampleUserData extends BaseEntity{
+
+}

12ece84bec9e2093e311a02626aa020cbe70c6a0
YOOJIIN <yoojiin0328@naver.com>
2025-10-14 00:39:53 +0900

    feat: @CurrentUser 주입(ArgumentResolver) 추가

---
+++ medinyang/src/main/java/oldman/medinyang/annotation/CurrentUser.java
@@ -0,0 +1,9 @@
+package oldman.medinyang.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CurrentUser {
+}
